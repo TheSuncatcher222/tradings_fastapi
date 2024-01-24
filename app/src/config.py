9f8b62befd8b9d72c6a862a31d7deb6b8a74efdd
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2024-01-24 10:36:18 +0300

    Add project config file

---
+++ app/src/config.py
@@ -0,0 +1,151 @@
+"""
+Модуль с настройками сервиса.
+"""
+
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+"""Настройки базы данных."""
+
+
+DB_HOST: str = os.getenv('DB_HOST')
+
+DB_NAME: str = os.getenv('POSTGRES_DB')
+
+DB_PASS: str = os.getenv('POSTGRES_PASSWORD')
+
+DB_PORT: str = os.getenv('DB_PORT')
+
+DB_USER: str = os.getenv('POSTGRES_USER')
+
+REDIS_HOST: str = os.getenv('REDIS_HOST')
+
+REDIS_PORT: str = os.getenv('REDIS_PORT')
+
+
+"""Настройки безопасности."""
+
+
+DOMAIN_IP: str = os.getenv('DOMAIN_IP')
+
+DOMAIN_NAME: str = os.getenv('DOMAIN_NAME')
+
+HASH_NAME: str = os.getenv('HASH_NAME')
+
+JWT_ALGORITHM: str = os.getenv('JWT_ALGORITHM')
+
+JWT_ACCESS_EXPIRATION_SEC: str = int(os.getenv('JWT_ACCESS_EXPIRATION_SEC'))
+
+JWT_REFRESH_EXPIRATION_SEC: str = int(os.getenv('JWT_REFRESH_EXPIRATION_SEC'))
+
+JWT_TYPE_ACCESS: str = 'access'
+
+JWT_TYPE_REFRESH: str = 'refresh'
+
+PASS_ENCODE: str = os.getenv('PASS_ENCODE')
+
+SALT: bytes = os.getenv('SALT').encode(PASS_ENCODE)
+
+SECRET_KEY: str = os.getenv('SECRET_KEY')
+
+ITERATIONS: int = int(os.getenv('ITERATIONS'))
+
+
+"""Настройки моделей ORM в базе данных."""
+
+
+TABLE_FEEDBACK: str = 'table_feedback'
+
+TABLE_USER: str = 'table_user'
+
+
+"""Настройки почтового клиента SMTP."""
+
+
+SMTP_HOST: str = os.getenv('SMTP_HOST')
+
+SMTP_PASSWORD: str = os.getenv('SMTP_PASSWORD')
+
+SMTP_PORT: str = os.getenv('SMTP_PORT')
+
+SMTP_USER: str = os.getenv('SMTP_USER')
+
+SUPPORT_EMAIL_TO: str = os.getenv('SUPPORT_EMAIL_TO')
+
+USE_SSL: bool = bool(os.getenv('USE_SSL').lower() == 'true')
+
+USE_TSL: bool = bool(os.getenv('USE_SSL').lower() == 'true')
+
+
+"""Настройки сервиса."""
+
+
+ASGI_PORT: int = int(os.getenv('ASGI_PORT'))
+
+DEBUG: str = os.getenv('DEBUG')
+
+if DEBUG == 'True':
+    DEBUG: bool = True
+    # Параметр "echo" в AsyncEngine, который выводит логи отладки в консоль.
+    DEBUG_DB: bool = True
+    # Количество работающих worker внутри сервиса.
+    WORKERS_AMOUNT: int = 1
+else:
+    DEBUG: bool = False
+    DEBUG_DB: bool = False
+    WORKERS_AMOUNT: int = int(os.getenv('WORKERS_AMOUNT'))
+
+
+def generate_json_error_content(
+        detail: str,
+        type_resp: str,
+        loc: list[str],
+        type_err: str
+):
+    """
+    Генерирует content для JSON ответа при возникновении ошибки
+    по формату стандартных ошибок FastAPI.
+    """
+    return {
+        'detail': detail,
+        'type': type_resp,
+        'ctx': {
+            'loc': loc,
+            'msg': detail,
+            'type': type_err,
+            'err_code': detail.lower().replace(' ', '_')
+        }
+    }
+
+
+JSON_ERR_CREDENTIALS_INVALID_EXPIRED: dict[str, any] = generate_json_error_content(
+    detail='Токен недействителен или срок его действия истек',
+    type_resp='token_not_valid',
+    loc=['header', 'Authorization'],
+    type_err='value_error'
+)
+
+JSON_ERR_CREDENTIALS_TYPE: dict[str, any] = generate_json_error_content(
+    detail='Данный токен недействителен',
+    type_resp='token_not_valid',
+    loc=['header', 'Authorization'],
+    type_err='value_error'
+)
+
+JSON_ERR_EMAIL_OR_PASS_INVALID: dict[str, any] = generate_json_error_content(
+    detail='Адрес электронной почты и(или) пароль недействительны',
+    type_resp='validation_error',
+    loc=['body', 'email', 'password'],
+    type_err='value_error',
+)
+
+JSON_ERR_EMAIL_IS_ALREADY_REGISTERED: dict[str, any] = generate_json_error_content(
+    detail='Пользователь с таким адресом электронной почты уже зарегистрирован',
+    type_resp='validation_error',
+    loc=['body', 'email'],
+    type_err='value_error',
+)
