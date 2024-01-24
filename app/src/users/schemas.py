5e556b523c28ac71ab2464676c693bf1f0048de2
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2024-01-24 10:41:56 +0300

    Add UserRepresent

---
+++ app/src/users/schemas.py
@@ -0,0 +1,21 @@
+"""
+Модуль со схемами валидации данных через Pydantic в приложении "users".
+"""
+
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserRepresent(BaseModel):
+    """Схема представления пользователя."""
+
+    id: int
+    name_first: str
+    name_last: str
+    email: EmailStr
+    phone: Optional[str] = None
+    telegram_username: Optional[str] = None
+    account_balance: int
+    reg_date: datetime
