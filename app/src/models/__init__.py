23723e43fad91bc0f860cecbe28f12d80a758fea
Kirill Svidunovich <svidunovich.spb@picassodg.ru>
2024-04-04 16:43:50 +0300

    Register User, UserSalesman and admin models

---
+++ app/src/models/__init__.py
@@ -0,0 +1,13 @@
+from src.models.user import (
+    User, UserAdmin,
+    UserSalesman, UserSalesmanAdmin,
+)
+
+__all__ = (
+    # Models:
+    #   - user
+    'User',
+    'UserAdmin',
+    'UserSalesman',
+    'UserSalesmanAdmin',
+)
