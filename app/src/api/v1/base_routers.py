6493106dac748af0ca5d072794553ab64601c5f7
Kirill Svidunovich <svidunovich.spb@picassodg.ru>
2024-04-05 10:01:31 +0300

    Register router_auth, router_users

--- app/src/api/v1/base_routers.py
+++ app/src/api/v1/base_routers.py
@@ -2,4 +2,17 @@
 
 from fastapi import APIRouter
 
+from src.api.v1.routers.auth import router_auth
+from src.api.v1.routers.user import router_users
+
 router_api_v1: APIRouter = APIRouter()
+
+ROUTERS: list[APIRouter] = [
+    router_auth,
+    router_users,
+]
+
+for router in ROUTERS:
+    router_api_v1.include_router(
+        router=router,
+    )
