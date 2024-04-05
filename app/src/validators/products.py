d40b68d9bb8f05da437ad42025ceabe726ed6b84
Kirill Svidunovich <svidunovich.spb@picassodg.ru>
2024-04-05 16:32:18 +0300

    Add constants

---
+++ app/src/validators/products.py
@@ -0,0 +1,8 @@
+"""
+Модуль с валидаторами полей моделей базы данных приложения "product".
+"""
+
+PRODUCT_TITLE_LEN: int = 50
+PRODUCT_DESCRIPTION_LEN: int = 200
+
+PRODUCT_CATEGORY_TITLE_LEN: int = 50
