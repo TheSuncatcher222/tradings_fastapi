ace0c49e36f51f10c8a4d348d80e6566a1928481
Kirill Svidunovich <svidunovich.spb@picassodg.ru>
2024-04-12 11:48:11 +0300

    Exclude email_confirm functions

--- app/src/utils/auth.py
+++ app/src/utils/auth.py
@@ -10,12 +10,9 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
 from src.api.v1.crud.user import user_v1_crud
-from src.celery_app.celery_app import PRIOR_MEDIUM
-from src.celery_app.auth.tasks import send_email_confirm_code_to_email
 from src.config.config import settings
 from src.database.database import AsyncSession, get_async_session
 from src.models.user import User
-from src.utils.itsdangerous import dangerous_token_generate
 from src.utils.jwt import jwt_decode
 
 oauth2_scheme: OAuth2PasswordBearer = OAuth2PasswordBearer(tokenUrl='auth/login')
@@ -74,36 +71,6 @@ async def get_current_user(
         session=session,
     )
     return user
-
-
-async def generate_email_confirm_code(user_id: int) -> str:
-    """Генерирует код подтверждения почты пользователя."""
-    return await dangerous_token_generate(data={'user_id': user_id})
-
-
-async def send_email_confirm_code(
-    user_id: int,
-    user_email: str,
-    user_full_name: str,
-) -> None:
-    """Ставит задачу Celery для отправки ссылки подтверждения электронной почты."""
-    confirm_code: str = await generate_email_confirm_code(user_id=user_id)
-
-    if settings.DEBUG_EMAIL:
-        user_email: str = settings.SUPPORT_EMAIL_TO
-        url_email_confirm: str = (
-            f'http://127.0.0.1:8000/api/v1/auth/email-confirm/{confirm_code}'
-        )
-    else:
-        url_email_confirm: str = (
-            f'https://{settings.DOMAIN_NAME}/api/v1/auth/email-confirm/{confirm_code}'
-        )
-
-    send_email_confirm_code_to_email.apply_async(
-        args=(user_full_name, url_email_confirm, user_email),
-        priority=PRIOR_MEDIUM,
-    )
-    return
 
 
 async def _get_jwt_payload(token: str) -> dict[str, any]:
