d8559ab58efeecf39b167f3dbfe7a15b212619d5
Kirill Svidunovich <svidunovich.spb@picassodg.ru>
2024-04-12 13:20:44 +0300

    Register router_feedback

--- app/src/api/v1/base_routers.py
+++ app/src/api/v1/base_routers.py
@@ -3,12 +3,14 @@
 from fastapi import APIRouter
 
 from src.api.v1.routers.auth import router_auth
+from src.api.v1.routers.feedback import router_feedback
 from src.api.v1.routers.user import router_users
 
 router_api_v1: APIRouter = APIRouter()
 
 ROUTERS: list[APIRouter] = [
     router_auth,
+    router_feedback,
     router_users,
 ]
 
