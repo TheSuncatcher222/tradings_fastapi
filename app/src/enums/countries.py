7ae66fda43d3e0fbd223c87fe66020523bee1128
Kirill Svidunovich <svidunovich.spb@picassodg.ru>
2024-04-25 17:54:49 +0300

    Start Country model

---
+++ app/src/enums/countries.py
@@ -0,0 +1,985 @@
+"""
+Список названий всех стран мира.
+"""
+
+
+class Country():
+    """
+    Список названий всех старн мира на русском языке
+    """
+
+    ...
+
+
+"""
+Afghanistan
+Albania
+Algeria
+Andorra
+Angola
+Antigua and Barbuda
+Argentina
+Armenia
+Australia
+Austria
+Azerbaijan
+The Bahamas
+Bahrain
+Bangladesh
+Barbados
+Belarus
+Belgium
+Belize
+Benin
+Bhutan
+Bolivia
+Bosnia and Herzegovina
+Botswana
+Brazil
+Brunei
+Bulgaria
+Burkina Faso
+Burundi
+Cabo Verde
+Cambodia
+Cameroon
+Canada
+Central African Republic
+Chad
+Chile
+China
+Colombia
+Comoros
+Congo, Democratic Republic of the
+Congo, Republic of the
+Costa Rica
+Côte d’Ivoire
+Croatia
+Cuba
+Cyprus
+Czech Republic
+Denmark
+Djibouti
+Dominica
+Dominican Republic
+East Timor (Timor-Leste)
+Ecuador
+Egypt
+El Salvador
+Equatorial Guinea
+Eritrea
+Estonia
+Eswatini
+Ethiopia
+Fiji
+Finland
+France
+Gabon
+The Gambia
+Georgia
+Germany
+Ghana
+Greece
+Grenada
+Guatemala
+Guinea
+Guinea-Bissau
+Guyana
+Haiti
+Honduras
+Hungary
+Iceland
+India
+Indonesia
+Iran
+Iraq
+Ireland
+Israel
+Italy
+Jamaica
+Japan
+Jordan
+Kazakhstan
+Kenya
+Kiribati
+Korea, North
+Korea, South
+Kosovo
+Kuwait
+Kyrgyzstan
+Laos
+Latvia
+Lebanon
+Lesotho
+Liberia
+Libya
+Liechtenstein
+Lithuania
+Luxembourg
+Madagascar
+Malawi
+Malaysia
+Maldives
+Mali
+Malta
+Marshall Islands
+Mauritania
+Mauritius
+Mexico
+Micronesia, Federated States of
+Moldova
+Monaco
+Mongolia
+Montenegro
+Morocco
+Mozambique
+Myanmar (Burma)
+Namibia
+Nauru
+Nepal
+Netherlands
+New Zealand
+Nicaragua
+Niger
+Nigeria
+North Macedonia
+Norway
+Oman
+Pakistan
+Palau
+Panama
+Papua New Guinea
+Paraguay
+Peru
+Philippines
+Poland
+Portugal
+Qatar
+Romania
+Russia
+Rwanda
+Saint Kitts and Nevis
+Saint Lucia
+Saint Vincent and the Grenadines
+Samoa
+San Marino
+Sao Tome and Principe
+Saudi Arabia
+Senegal
+Serbia
+Seychelles
+Sierra Leone
+Singapore
+Slovakia
+Slovenia
+Solomon Islands
+Somalia
+South Africa
+Spain
+Sri Lanka
+Sudan
+Sudan, South
+Suriname
+Sweden
+Switzerland
+Syria
+Taiwan
+Tajikistan
+Tanzania
+Thailand
+Togo
+Tonga
+Trinidad and Tobago
+Tunisia
+Turkey
+Turkmenistan
+Tuvalu
+Uganda
+Ukraine
+United Arab Emirates
+United Kingdom
+United States
+Uruguay
+Uzbekistan
+Vanuatu
+Vatican City
+Venezuela
+Vietnam
+Yemen
+Zambia
+Zimbabwe
+"""
+
+
+"""
+AFGHANISTAN: Афганистан
+ALBANIA: Албания
+ALGERIA: Алжир
+ANDORRA: Андорра
+ANGOLA: Ангола
+ANTIGUA_AND_BARBUDA: Антигуа и Барбуда
+ARGENTINA: Аргентина
+ARMENIA: Армения
+AUSTRALIA: Австралия
+AUSTRIA: Австрия
+AZERBAIJAN: Азербайджан
+THE_BAHAMAS: Багамские Острова
+BAHRAIN: Бахрейн
+BANGLADESH: Бангладеш
+BARBADOS: Барбадос
+BELARUS: Беларусь
+BELGIUM: Бельгия
+BELIZE: Белиз
+BENIN: Бенин
+BHUTAN: Бутан
+BOLIVIA: Боливия
+BOSNIA_AND_HERZEGOVINA: Босния и Герцеговина
+BOTSWANA: Ботсвана
+BRAZIL: Бразилия
+BRUNEI: Бруней
+BULGARIA: Болгария
+BURKINA_FASO: Буркина-Фасо
+BURUNDI: Бурунди
+CABO_VERDE: Кабо-Верде
+CAMBODIA: Камбоджа
+CAMEROON: Камерун
+CANADA: Канада
+CENTRAL_AFRICAN_REPUBLIC: Центральноафриканская Республика
+CHAD: Чад
+CHILE: Чили
+CHINA: Китай
+COLOMBIA: Колумбия
+COMOROS: Коморские Острова
+CONGO_DEMOCRATIC_REPUBLIC_OF_THE: Демократическая Республика Конго
+CONGO_REPUBLIC_OF_THE: Республика Конго
+COSTA_RICA: Коста-Рика
+COTE_D_IVOIRE: Кот-д’Ивуар
+CROATIA: Хорватия
+CUBA: Куба
+CYPRUS: Кипр
+CZECH_REPUBLIC: Чешская Республика
+DENMARK: Дания
+DJIBOUTI: Джибути
+DOMINICA: Доминика
+DOMINICAN_REPUBLIC: Доминиканская Республика
+EAST_TIMOR: Восточный Тимор
+ECUADOR: Эквадор
+EGYPT: Египет
+EL_SALVADOR: Сальвадор
+EQUATORIAL_GUINEA: Экваториальная Гвинея
+ERITREA: Эритрея
+ESTONIA: Эстония
+ESWATINI: Эсватини
+ETHIOPIA: Эфиопия
+FIJI: Фиджи
+FINLAND: Финляндия
+FRANCE: Франция
+GABON: Габон
+THE_GAMBIA: Гамбия
+GEORGIA: Грузия
+GERMANY: Германия
+GHANA: Гана
+GREECE: Греция
+GRENADA: Гренада
+GUATEMALA: Гватемала
+GUINEA: Гвинея
+GUINEA_BISSAU: Гвинея-Бисау
+GUYANA: Гайана
+HAITI: Гаити
+HONDURAS: Гондурас
+HUNGARY: Венгрия
+ICELAND: Исландия
+INDIA: Индия
+INDONESIA: Индонезия
+IRAN: Иран
+IRAQ: Ирак
+IRELAND: Ирландия
+ISRAEL: Израиль
+ITALY: Италия
+JAMAICA: Ямайка
+JAPAN: Япония
+JORDAN: Иордания
+KAZAKHSTAN: Казахстан
+KENYA: Кения
+KIRIBATI: Кирибати
+KOREA_NORTH: Корея, Северная
+KOREA_SOUTH: Корея, Южная
+KOSOVO: Косово
+KUWAIT: Кувейт
+KYRGYZSTAN: Киргизия
+LAOS: Лаос
+LATVIA: Латвия
+LEBANON: Ливан
+LESOTHO: Лесото
+LIBERIA: Либерия
+LIBYA: Ливия
+LIECHTENSTEIN: Лихтенштейн
+LITHUANIA: Литва
+LUXEMBOURG: Люксембург
+MADAGASCAR: Мадагаскар
+MALAWI: Малави
+MALAYSIA: Малайзия
+MALDIVES: Мальдивы
+MALI: Мали
+MALTA: Мальта
+MARSHALL_ISLANDS: Маршалловы Острова
+MAURITANIA: Мавритания
+MAURITIUS: Маврикий
+MEXICO: Мексика
+MICRONESIA_FEDERATED_STATES_OF: Микронезия, Федеративные Штаты
+MOLDOVA: Молдова
+MONACO: Монако
+MONGOLIA: Монголия
+MONTENEGRO: Черногория
+MOROCCO: Марокко
+MOZAMBIQUE: Мозамбик
+MYANMAR_BURMA: Мьянма (Бирма)
+NAMIBIA: Намибия
+NAURU: Науру
+NEPAL: Непал
+NETHERLANDS: Нидерланды
+NEW_ZEALAND: Новая Зеландия
+NICARAGUA: Никарагуа
+NIGER: Нигер
+NIGERIA: Нигерия
+NORTH_MACEDONIA: Северная Македония
+NORWAY: Норвегия
+OMAN: Оман
+PAKISTAN: Пакистан
+PALAU: Палау
+PANAMA: Панама
+PAPUA_NEW_GUINEA: Папуа-Новая Гвинея
+PARAGUAY: Парагвай
+PERU: Перу
+PHILIPPINES: Филиппины
+POLAND: Польша
+PORTUGAL: Португалия
+QATAR: Катар
+ROMANIA: Румыния
+RUSSIA: Россия
+RWANDA: Руанда
+SAINT_KITTS_AND_NEVIS: Сент-Китс и Невис
+SAINT_LUCIA: Сент-Люсия
+SAINT_VINCENT AND THE GRENADINES: Сент-Винсент и Гренадины
+SAMOA: Самоа
+SAN_MARINO: Сан-Марино
+SAO_TOME AND PRINCIPE: Сан-Томе и Принсипи
+SAUDI_ARABIA: Саудовская Аравия
+SENEGAL: Сенегал
+SERBIA: Сербия
+SEYCHELLES: Сейшельские Острова
+SIERRA_LEONE: Сьерра-Леоне
+SINGAPORE: Сингапур
+SLOVAKIA: Словакия
+SLOVENIA: Словения
+SOLOMON_ISLANDS: Соломоновы Острова
+SOMALIA: Сомали
+SOUTH_AFRICA: Южная Африка
+SPAIN: Испания
+SRI LANKA: Шри-Ланка
+SUDAN: Судан
+SUDAN_SOUTH: Южный Судан
+SURINAME: Суринам
+SWEDEN: Швеция
+SWITZERLAND: Швейцария
+SYRIA: Сирия
+TAIWAN: Тайвань
+TAJIKISTAN: Таджикистан
+TANZANIA: Танзания
+THAILAND: Таиланд
+TOGO: Того
+TONGA: Тонга
+TRINIDAD_AND_TOBAGO: Тринидад и Тобаго
+TUNISIA: Тунис
+TURKEY: Турция
+TURKMENISTAN: Туркменистан
+TUVALU: Тувалу
+UGANDA: Уганда
+UKRAINE: Украина
+UNITED_ARAB_EMIRATES: Объединенные Арабские Эмираты
+UNITED_KINGDOM: Великобритания
+UNITED_STATES: Соединенные Штаты
+URUGUAY: Уругвай
+UZBEKISTAN: Узбекистан
+VANUATU: Вануату
+VATICAN_CITY: Ватикан
+VENEZUELA: Венесуэла
+VIETNAM: Вьетнам
+YEMEN: Йемен
+ZAMBIA: Замбия
+ZIMBABWE: Зимбабве
+"""
+
+"""
+countries_dict = {
+    "AFGHANISTAN": {
+        "id": 1,
+        "title_eng": "Afghanistan",
+        "title_rus": "Афганистан",
+        "code": "AFG"
+    },
+    "ALBANIA": {
+        "id": 2,
+        "title_eng": "Albania",
+        "title_rus": "Албания",
+        "code": "ALB"
+    },
+    "ALGERIA": {
+        "id": 3,
+        "title_eng": "Algeria",
+        "title_rus": "Алжир",
+        "code": "DZA"
+    },
+    "ANDORRA": {
+        "id": 4,
+        "title_eng": "Andorra",
+        "title_rus": "Андорра",
+        "code": "AND"
+    },
+    "ANGOLA": {
+        "id": 5,
+        "title_eng": "Angola",
+        "title_rus": "Ангола",
+        "code": "AGO"
+    },
+    "ANTIGUA AND BARBUDA": {
+        "id": 6,
+        "title_eng": "Antigua and Barbuda",
+        "title_rus": "Антигуа и Барбуда",
+        "code": "ATG"
+    },
+    "ARGENTINA": {
+        "id": 7,
+        "title_eng": "Argentina",
+        "title_rus": "Аргентина",
+        "code": "ARG"
+    },
+    "ARMENIA": {
+        "id": 8,
+        "title_eng": "Armenia",
+        "title_rus": "Армения",
+        "code": "ARM"
+    },
+    "AUSTRALIA": {
+        "id": 9,
+        "title_eng": "Australia",
+        "title_rus": "Австралия",
+        "code": "AUS"
+    },
+    "AUSTRIA": {
+        "id": 10,
+        "title_eng": "Austria",
+        "title_rus": "Австрия",
+        "code": "AUT"
+    },
+    "AZERBAIJAN": {
+        "id": 11,
+        "title_eng": "Azerbaijan",
+        "title_rus": "Азербайджан",
+        "code": "AZE"
+    },
+    "THE BAHAMAS": {
+        "id": 12,
+        "title_eng": "The Bahamas",
+        "title_rus": "Багамы",
+        "code": "BHS"
+    },
+    "BAHRAIN": {
+        "id": 13,
+        "title_eng": "Bahrain",
+        "title_rus": "Бахрейн",
+        "code": "BHR"
+    },
+    "BANGLADESH": {
+        "id": 14,
+        "title_eng": "Bangladesh",
+        "title_rus": "Бангладеш",
+        "code": "BGD"
+    },
+    "BARBADOS": {
+        "id": 15,
+        "title_eng": "Barbados",
+        "title_rus": "Барбадос",
+        "code": "BRB"
+    },
+    "BELARUS": {
+        "id": 16,
+        "title_eng": "Belarus",
+        "title_rus": "Беларусь",
+        "code": "BLR"
+    },
+    "BELGIUM": {
+        "id": 17,
+        "title_eng": "Belgium",
+        "title_rus": "Бельгия",
+        "code": "BEL"
+    },
+    "BELIZE": {
+        "id": 18,
+        "title_eng": "Belize",
+        "title_rus": "Белиз",
+        "code": "BLZ"
+    },
+    "BENIN": {
+        "id": 19,
+        "title_eng": "Benin",
+        "title_rus": "Бенин",
+        "code": "BEN"
+    },
+    "BHUTAN": {
+        "id": 20,
+        "title_eng": "Bhutan",
+        "title_rus": "Бутан",
+        "code": "BTN"
+    },
+    "BOLIVIA": {
+        "id": 21,
+        "title_eng": "Bolivia",
+        "title_rus": "Боливия",
+        "code": "BOL"
+    },
+    "BOSNIA AND HERZEGOVINA": {
+        "id": 22,
+        "title_eng": "Bosnia and Herzegovina",
+        "title_rus": "Босния и Герцеговина",
+        "code": "BIH"
+    },
+    "BOTSWANA": {
+        "id": 23,
+        "title_eng": "Botswana",
+        "title_rus": "Ботсвана",
+        "code": "BWA"
+    },
+    "BRAZIL": {
+        "id": 24,
+        "title_eng": "Brazil",
+        "title_rus": "Бразилия",
+        "code": "BRA"
+    },
+    "BRUNEI": {
+        "id": 25,
+        "title_eng": "Brunei",
+        "title_rus": "Бруней",
+        "code": "BRN"
+    },
+    "BULGARIA": {
+        "id": 26,
+        "title_eng": "Bulgaria",
+        "title_rus": "Болгария",
+        "code": "BGR"
+    },
+    "BURKINA FASO": {
+        "id": 27,
+        "title_eng": "Burkina Faso",
+        "title_rus": "Буркина-Фасо",
+        "code": "BFA"
+    },
+    "BURUNDI": {
+        "id": 28,
+        "title_eng": "Burundi",
+        "title_rus": "Бурунди",
+        "code": "BDI"
+    },
+    "CABO VERDE": {
+        "id": 29,
+        "title_eng": "Cabo Verde",
+        "title_rus": "Кабо-Верде",
+        "code": "CPV"
+    },
+    "CAMBODIA": {
+        "id": 30,
+        "title_eng": "Cambodia",
+        "title_rus": "Камбоджа",
+        "code": "KHM"
+    },
+    "CAMEROON": {
+        "id": 31,
+        "title_eng": "Cameroon",
+        "title_rus": "Камерун",
+        "code": "CMR"
+    },
+    "CANADA": {
+        "id": 32,
+        "title_eng": "Canada",
+        "title_rus": "Канада",
+        "code": "CAN"
+    },
+    "CENTRAL AFRICAN REPUBLIC": {
+        "id": 33,
+        "title_eng": "Central African Republic",
+        "title_rus": "Центральноафриканская Республика",
+        "code": "CAF"
+    },
+    "CHAD": {
+        "id": 34,
+        "title_eng": "Chad",
+        "title_rus": "Чад",
+        "code": "TCD"
+    },
+    "CHILE": {
+        "id": 35,
+        "title_eng": "Chile",
+        "title_rus": "Чили",
+        "code": "CHL"
+    },
+    "CHINA": {
+        "id": 36,
+        "title_eng": "China",
+        "title_rus": "Китай",
+        "code": "CHN"
+    },
+    "COLOMBIA": {
+        "id": 37,
+        "title_eng": "Colombia",
+        "title_rus": "Колумбия",
+        "code": "COL"
+    },
+    "COMOROS": {
+        "id": 38,
+        "title_eng": "Comoros",
+        "title_rus": "Коморы",
+        "code": "COM"
+    },
+    "CONGO, DEMOCRATIC REPUBLIC OF THE": {
+        "id": 39,
+        "title_eng": "Congo, Democratic Republic of the",
+        "title_rus": "Конго, Демократическая Республика",
+        "code": "COD"
+    },
+    "CONGO, REPUBLIC OF THE": {
+        "id": 40,
+        "title_eng": "Congo, Republic of the",
+        "title_rus": "Конго, Республика",
+        "code": "COG"
+    },
+    "COSTA RICA": {
+        "id": 41,
+        "title_eng": "Costa Rica",
+        "title_rus": "Коста-Рика",
+        "code": "CRI"
+    },
+    "CÔTE D’IVOIRE": {
+        "id": 42,
+        "title_eng": "Côte d’Ivoire",
+        "title_rus": "Кот-д'Ивуар",
+        "code": "CIV"
+    },
+    "CROATIA": {
+        "id": 43,
+        "title_eng": "Croatia",
+        "title_rus": "Хорватия",
+        "code": "HRV"
+    },
+    "CUBA": {
+        "id": 44,
+        "title_eng": "Cuba",
+        "title_rus": "Куба",
+        "code": "CUB"
+    },
+    "CYPRUS": {
+        "id": 45,
+        "title_eng": "Cyprus",
+        "title_rus": "Кипр",
+        "code": "CYP"
+    },
+    "CZECH REPUBLIC": {
+        "id": 46,
+        "title_eng": "Czech Republic",
+        "title_rus": "Чешская Республика",
+        "code": "CZE"
+    },
+    "DENMARK": {
+        "id": 47,
+        "title_eng": "Denmark",
+        "title_rus": "Дания",
+        "code": "DNK"
+    },
+    "DJIBOUTI": {
+        "id": 48,
+        "title_eng": "Djibouti",
+        "title_rus": "Джибути",
+        "code": "DJI"
+    },
+    "DOMINICA": {
+        "id": 49,
+        "title_eng": "Dominica",
+        "title_rus": "Доминика",
+        "code": "DMA"
+    },
+    "DOMINICAN REPUBLIC": {
+        "id": 50,
+        "title_eng": "Dominican Republic",
+        "title_rus": "Доминиканская Республика",
+        "code": "DOM"
+    },
+    "EAST TIMOR (TIMOR-LESTE)": {
+        "id": 51,
+        "title_eng": "East Timor (Timor-Leste)",
+        "title_rus": "Восточный Тимор (Тимор-Лесте)",
+        "code": "TLS"
+    },
+    "ECUADOR": {
+        "id": 52,
+        "title_eng": "Ecuador",
+        "title_rus": "Эквадор",
+        "code": "ECU"
+    },
+    "EGYPT": {
+        "id": 53,
+        "title_eng": "Egypt",
+        "title_rus": "Египет",
+        "code": "EGY"
+    },
+    "EL SALVADOR": {
+        "id": 54,
+        "title_eng": "El Salvador",
+        "title_rus": "Сальвадор",
+        "code": "SLV"
+    },
+    "EQUATORIAL GUINEA": {
+        "id": 55,
+        "title_eng": "Equatorial Guinea",
+        "title_rus": "Экваториальная Гвинея",
+        "code": "GNQ"
+    },
+    "ERITREA": {
+        "id": 56,
+        "title_eng": "Eritrea",
+        "title_rus": "Эритрея",
+        "code": "ERI"
+    },
+    "ESTONIA": {
+        "id": 57,
+        "title_eng": "Estonia",
+        "title_rus": "Эстония",
+        "code": "EST"
+    },
+    "ESWATINI": {
+        "id": 58,
+        "title_eng": "Eswatini",
+        "title_rus": "Эсватини",
+        "code": "SWZ"
+    },
+    "ETHIOPIA": {
+        "id": 59,
+        "title_eng": "Ethiopia",
+        "title_rus": "Эфиопия",
+        "code": "ETH"
+    },
+    "FIJI": {
+        "id": 60,
+        "title_eng": "Fiji",
+        "title_rus": "Фиджи",
+        "code": "FJI"
+    },
+    "FINLAND": {
+        "id": 61,
+        "title_eng": "Finland",
+        "title_rus": "Финляндия",
+        "code": "FIN"
+    },
+    "FRANCE": {
+        "id": 62,
+        "title_eng": "France",
+        "title_rus": "Франция",
+        "code": "FRA"
+    },
+    "GABON": {
+        "id": 63,
+        "title_eng": "Gabon",
+        "title_rus": "Габон",
+        "code": "GAB"
+    },
+        "THE GAMBIA": {
+        "id": 64,
+        "title_eng": "The Gambia",
+        "title_rus": "Гамбия",
+        "code": "GMB"
+    },
+    "GEORGIA": {
+        "id": 65,
+        "title_eng": "Georgia",
+        "title_rus": "Грузия",
+        "code": "GEO"
+    },
+    "GERMANY": {
+        "id": 66,
+        "title_eng": "Germany",
+        "title_rus": "Германия",
+        "code": "DEU"
+    },
+    "GHANA": {
+        "id": 67,
+        "title_eng": "Ghana",
+        "title_rus": "Гана",
+        "code": "GHA"
+    },
+    "GREECE": {
+        "id": 68,
+        "title_eng": "Greece",
+        "title_rus": "Греция",
+        "code": "GRC"
+    },
+    "GRENADA": {
+        "id": 69,
+        "title_eng": "Grenada",
+        "title_rus": "Гренада",
+        "code": "GRD"
+    },
+    "GUATEMALA": {
+        "id": 70,
+        "title_eng": "Guatemala",
+        "title_rus": "Гватемала",
+        "code": "GTM"
+    },
+    "GUINEA": {
+        "id": 71,
+        "title_eng": "Guinea",
+        "title_rus": "Гвинея",
+        "code": "GIN"
+    },
+    "GUINEA-BISSAU": {
+        "id": 72,
+        "title_eng": "Guinea-Bissau",
+        "title_rus": "Гвинея-Бисау",
+        "code": "GNB"
+    },
+    "GUYANA": {
+        "id": 73,
+        "title_eng": "Guyana",
+        "title_rus": "Гайана",
+        "code": "GUY"
+    },
+    "HAITI": {
+        "id": 74,
+        "title_eng": "Haiti",
+        "title_rus": "Гаити",
+        "code": "HTI"
+    },
+    "HONDURAS": {
+        "id": 75,
+        "title_eng": "Honduras",
+        "title_rus": "Гондурас",
+        "code": "HND"
+    },
+    "HUNGARY": {
+        "id": 76,
+        "title_eng": "Hungary",
+        "title_rus": "Венгрия",
+        "code": "HUN"
+    },
+    "ICELAND": {
+        "id": 77,
+        "title_eng": "Iceland",
+        "title_rus": "Исландия",
+        "code": "ISL"
+    },
+    "INDIA": {
+        "id": 78,
+        "title_eng": "India",
+        "title_rus": "Индия",
+        "code": "IND"
+    },
+    "INDONESIA": {
+        "id": 79,
+        "title_eng": "Indonesia",
+        "title_rus": "Индонезия",
+        "code": "IDN"
+    },
+    "IRAN": {
+        "id": 80,
+        "title_eng": "Iran",
+        "title_rus": "Иран",
+        "code": "IRN"
+    },
+    "IRAQ": {
+        "id": 81,
+        "title_eng": "Iraq",
+        "title_rus": "Ирак",
+        "code": "IRQ"
+    },
+    "IRELAND": {
+        "id": 82,
+        "title_eng": "Ireland",
+        "title_rus": "Ирландия",
+        "code": "IRL"
+    },
+    "ISRAEL": {
+        "id": 83,
+        "title_eng": "Israel",
+        "title_rus": "Израиль",
+        "code": "ISR"
+    },
+    "ITALY": {
+        "id": 84,
+        "title_eng": "Italy",
+        "title_rus": "Италия",
+        "code": "ITA"
+    },
+    "JAMAICA": {
+        "id": 85,
+        "title_eng": "Jamaica",
+        "title_rus": "Ямайка",
+        "code": "JAM"
+    },
+    "JAPAN": {
+        "id": 86,
+        "title_eng": "Japan",
+        "title_rus": "Япония",
+        "code": "JPN"
+    },
+    "JORDAN": {
+        "id": 87,
+        "title_eng": "Jordan",
+        "title_rus": "Иордания",
+        "code": "JOR"
+    },
+    "KAZAKHSTAN": {
+        "id": 88,
+        "title_eng": "Kazakhstan",
+        "title_rus": "Казахстан",
+        "code": "KAZ"
+    },
+    "KENYA": {
+        "id": 89,
+        "title_eng": "Kenya",
+        "title_rus": "Кения",
+        "code": "KEN"
+    },
+    "KIRIBATI": {
+        "id": 90,
+        "title_eng": "Kiribati",
+        "title_rus": "Кирибати",
+        "code": "KIR"
+    },
+    "KOREA, NORTH": {
+        "id": 91,
+        "title_eng": "Korea, North",
+        "title_rus": "Корея, Северная",
+        "code": "PRK"
+    },
+    "KOREA, SOUTH": {
+        "id": 92,
+        "title_eng": "Korea, South",
+        "title_rus": "Корея, Южная",
+        "code": "KOR"
+    },
+    "KOSOVO": {
+        "id": 93,
+        "title_eng": "Kosovo",
+        "title_rus": "Косово",
+        "code": "XKX"
+    },
+    "KUWAIT": {
+        "id": 94,
+        "title_eng": "Kuwait",
+        "title_rus": "Кувейт",
+        "code": "KWT"
+    },
+    "KYRGYZSTAN": {
+        "id": 95,
+        "title_eng": "Kyrgyzstan",
+        "title_rus": "Киргизия",
+        "code": "KGZ"
+    },
+"""
