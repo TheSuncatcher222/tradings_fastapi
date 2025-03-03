5a0231b4ca64ce4935122048df2d7e4c85371abf
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-03-03 16:20:38 +0300

    Refactor, add logging

--- app/src/utils/itsdangerous.py
+++ app/src/utils/itsdangerous.py
@@ -4,33 +4,53 @@
 Включает в себя функции работы с библиотекой ItsDangerous.
 """
 
-from itsdangerous import URLSafeTimedSerializer, BadSignature
+from itsdangerous import (
+    URLSafeTimedSerializer,
+    BadSignature,
+)
 
-from src.config.config import settings
+from app.src.config.config import (
+    TimeIntervals,
+    settings,
+)
+from app.src.utils.logger_json import (
+    Logger,
+    LoggerJsonAuth,
+)
+
+logger: Logger = LoggerJsonAuth
 
 SALT: bytes = (settings.SALT).encode(settings.PASS_ENCODE)
 
 
 async def dangerous_token_generate(data: dict[str, any]) -> str:
     """Генерирует уникальный токен, содержащий данные из параметров функции."""
-    serializer: URLSafeTimedSerializer = await _get_dangerous_serializer()
+    serializer: URLSafeTimedSerializer = await __get_dangerous_serializer()
     return serializer.dumps(obj=data)
 
 
-async def dangerous_token_verify(token, expiration: int = settings.ONE_DAY_SEC) -> any:
+async def dangerous_token_verify(token, expiration: int = TimeIntervals.SECONDS_IN_1_DAY) -> any:
     """
     Верифицирует и возвращает данные из уникального токена.
 
     Если токен невалидный - возвращает None.
     """
-    serializer: URLSafeTimedSerializer = await _get_dangerous_serializer()
+    serializer: URLSafeTimedSerializer = await __get_dangerous_serializer()
+
     try:
         return serializer.loads(token, max_age=expiration)
+
     except BadSignature:
+
+        logger.info(
+            msg=f'Token has itsdangerous.BadSignature.',
+            extra={'token': token},
+        )
+
         return None
 
 
-async def _get_dangerous_serializer() -> URLSafeTimedSerializer:
+async def __get_dangerous_serializer() -> URLSafeTimedSerializer:
     """Инициализирует сериализатор генерации токенов."""
     return URLSafeTimedSerializer(
         secret_key=settings.SECRET_KEY,
