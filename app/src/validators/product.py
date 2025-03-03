f53816ff56021192985ff7a979ac7265cb2ff6f0
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-03-03 16:13:43 +0300

    Add ProductParams

---
+++ app/src/validators/product.py
@@ -0,0 +1,11 @@
+"""
+Модуль с валидаторами приложения "product".
+"""
+
+
+class ProductParams:
+    """Класс с параметрами товаров."""
+
+    CATEGORY_TITLE_LEN_MAX: int = 50
+    DESCRIPTION_LEN_MAX: int = 200
+    TITLE_LEN_MAX: int = 50
--- app/src/validators/products.py
+++
@@ -1,8 +0,0 @@
-"""
-Модуль с валидаторами полей моделей базы данных приложения "product".
-"""
-
-PRODUCT_TITLE_LEN: int = 50
-PRODUCT_DESCRIPTION_LEN: int = 200
-
-PRODUCT_CATEGORY_TITLE_LEN: int = 50
