d5d066e9c45b8784873d409e8da13eeb69d27df1
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-03-05 12:00:35 +0300

    Add FeedbackV1CrudSync

--- app/src/api/v1/crud/feedback.py
+++ app/src/api/v1/crud/feedback.py
@@ -1,6 +1,7 @@
 """Модуль с классом CRUD запросов в базу данных для модели UsedPassResetToken."""
 
 from src.database.base_async_crud import BaseAsyncCrud
+from src.database.base_sync_crud import BaseSyncCrud
 from src.models.feedback import Feedback
 
 
@@ -10,3 +11,10 @@ class FeedbackV1Crud(BaseAsyncCrud):
 
 
 feedback_v1_crud = FeedbackV1Crud(model=Feedback)
+
+
+class FeedbackV1CrudSync(BaseSyncCrud):
+    """Класс CRUD запросов к базе данных к таблице Feedback."""
+
+
+feedback_v1_crud_sync = FeedbackV1CrudSync(model=Feedback)
