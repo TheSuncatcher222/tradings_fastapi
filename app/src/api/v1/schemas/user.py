af2658233345dcdf7dfad67a6a89957fc045f98e
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-03-05 12:09:37 +0300

    Refactor, update codestyle

--- app/src/api/v1/schemas/user.py
+++ app/src/api/v1/schemas/user.py
@@ -6,16 +6,20 @@ from datetime import datetime
 from decimal import Decimal
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr, validator
+from pydantic import (
+    BaseModel,
+    EmailStr,
+    field_validator,
+    model_validator,
+)
 
 from src.validators.user import (
+    UserParams,
     validate_user_company_name,
     validate_user_email,
     validate_user_password,
     validate_user_phone,
     validate_user_name,
-    USER_NAME_FIRST_ERROR,
-    USER_NAME_LAST_ERROR,
 )
 
 
@@ -28,35 +32,36 @@ class UserRepresent(BaseModel):
     email_is_confirmed: bool
     phone: Optional[str]
     phone_is_confirmed: bool
-    registration_datetime: datetime
+    datetime_registration: datetime
 
 
 class UserUpdate(BaseModel):
     """Схема обновления пользователя."""
+
     name_first: Optional[str] = None
     name_last: Optional[str] = None
     email: Optional[EmailStr] = None
     phone: Optional[str] = None
 
-    @validator('name_first')
+    @field_validator('name_first')
     def validate_name_first(cls, value: str) -> str:
         """
         Производит валидацию поля "name_first".
 
         Переводит символы поля в title регистр.
         """
-        return validate_user_name(value=value, err=USER_NAME_FIRST_ERROR)
+        return validate_user_name(value=value, err=UserParams.NAME_FIRST_ERROR)
 
-    @validator('name_last')
+    @field_validator('name_last')
     def validate_name_last(cls, value: str) -> str:
         """
         Производит валидацию поля "name_last".
 
         Переводит символы поля в title регистр.
         """
-        return validate_user_name(value=value, err=USER_NAME_LAST_ERROR)
+        return validate_user_name(value=value, err=UserParams.NAME_LAST_ERROR)
 
-    @validator('email')
+    @field_validator('email')
     def validate_email(cls, value: str) -> str:
         """
         Производит валидацию поля "email".
@@ -67,13 +72,13 @@ class UserUpdate(BaseModel):
         """
         return validate_user_email(value=value)
 
-    @validator('phone')
+    @field_validator('phone')
     def validate_phone(cls, value: str) -> str:
         """Производит валидацию поля "phone"."""
         return validate_user_phone(value=value)
 
 
-class UserRegister(BaseModel):
+class UserRegisterSchema(BaseModel):
     """Схема представления данных для регистрации пользователя."""
 
     name_first: str
@@ -86,25 +91,25 @@ class UserRegister(BaseModel):
     description: Optional[str] = None
     image: Optional[str] = None
 
-    @validator('name_first')
+    @field_validator('name_first')
     def validate_name_first(cls, value: str) -> str:
         """
         Производит валидацию поля "name_first".
 
         Переводит символы поля в title регистр.
         """
-        return validate_user_name(value=value, err=USER_NAME_FIRST_ERROR)
+        return validate_user_name(value=value, err=UserParams.NAME_FIRST_ERROR)
 
-    @validator('name_last')
+    @field_validator('name_last')
     def validate_name_last(cls, value: str) -> str:
         """
         Производит валидацию поля "name_last".
 
         Переводит символы поля в title регистр.
         """
-        return validate_user_name(value=value, err=USER_NAME_LAST_ERROR)
+        return validate_user_name(value=value, err=UserParams.NAME_LAST_ERROR)
 
-    @validator('email')
+    @field_validator('email')
     def validate_email(cls, value: str) -> str:
         """
         Производит валидацию поля "email":
@@ -113,12 +118,12 @@ class UserRegister(BaseModel):
         """
         return validate_user_email(value=value)
 
-    @validator('password')
+    @field_validator('password')
     def validate_password(cls, value: str) -> str:
         """Производит валидацию поля "password"."""
         return validate_user_password(value=value)
 
-    @validator('phone')
+    @field_validator('phone')
     def validate_phone(cls, value: str) -> str:
         """
         Производит валидацию поля "phone":
@@ -130,7 +135,7 @@ class UserRegister(BaseModel):
         """
         return validate_user_phone(value=value)
 
-    @validator('company_name', always=True)
+    @field_validator('company_name')
     def validate_company_name(cls, value: str, values: dict) -> str:
         """
         Производит валидацию поля "company_name".
@@ -142,7 +147,7 @@ class UserRegister(BaseModel):
             return None
         return validate_user_company_name(value=value)
 
-    @validator('description', always=True)
+    @field_validator('description')
     def validate_description(cls, value: str, values: dict) -> str:
         """
         Производит валидацию поля "description".
@@ -154,12 +159,11 @@ class UserRegister(BaseModel):
             return None
         if value is None:
             raise ValueError(
-                'Введите адрес компании'
+                'Введите адрес компании',
             )
         return value
 
-    # TODO. Добавить валидацию изображения.
-    @validator('image', always=True)
+    @field_validator('image')
     def validate_image(cls, value: str, values: dict) -> None:
         """
         Производит валидацию поля "image".
@@ -171,20 +175,17 @@ class UserRegister(BaseModel):
             return None
         return None
 
-    @validator('is_salesman')
-    def validate_is_organization(cls, value: str, values: dict) -> bool:
+    @model_validator(mode='before')
+    def validate_is_organization(cls, self: dict) -> dict:
         """
         Производит валидацию поля "is_salesman"
         и связанных с ним полей "company_name" и "description".
         """
-        if not all([values.get(key) for key in ('company_name', 'description')]):
+        if not all([self.get(key) for key in ('company_name', 'description')]):
             raise ValueError(
-                'Не заполнены все обязательные поля для организации: "company_name", "description"'
+                'Не заполнены все обязательные поля для организации: "company_name", "description"',
             )
         return True
-
-
-"""Схемы представления пользователей-продавцов."""
 
 
 class UserSalesmanCompanyForProductRepresent(BaseModel):
