fd2486010704e9d48bcbb1af790c9e32766970fb
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-03-05 12:02:15 +0300

    Refactor

--- app/src/api/v1/routers/feedback.py
+++ app/src/api/v1/routers/feedback.py
@@ -2,12 +2,19 @@
 Модуль с эндпоинтами приложения "feedback".
 """
 
-from fastapi import APIRouter, Depends, status
+from fastapi import (
+    APIRouter,
+    Depends,
+    status,
+)
 from fastapi.responses import JSONResponse
 
 from src.api.v1.crud.feedback import feedback_v1_crud
 from src.api.v1.schemas.feedback import FeedbackSend
-from src.database.database import AsyncSession, get_async_session
+from src.database.database import (
+    AsyncSession,
+    get_async_session,
+)
 
 router_feedback: APIRouter = APIRouter(
     prefix='/feedbacks',
