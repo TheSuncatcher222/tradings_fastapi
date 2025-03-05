a587d28cbdf5936166329b721877e0b779e968cd
Kirill Svidunovich <thesuncatcher222@gmail.com>
2025-03-05 12:21:40 +0300

    Add UserParams, refactor, update codestyle

--- app/src/validators/user.py
+++ app/src/validators/user.py
@@ -1,77 +1,80 @@
 """
-Модуль с валидаторами полей моделей базы данных приложения "user".
+Модуль с валидаторами приложения "user".
 """
 
 from re import fullmatch
 
-NL: str = '\n'
 
-COMPANY_DESCRIPTION_MAX: int = 250
-COMPANY_NAME_MAX: int = 30
+class CompanyParams:
+    """Класс с параметрами компаний."""
 
-# INFO: в pydantic.EmailStr разрешенные длины строк составляют 64@63.63
-USER_EMAIL_LEN: int = 64 + 63 + 63
-USER_HASH_PASS_LEN: int = 256
-USER_PHONE_LEN: int = 20
-USER_USERNAME_LEN: int = 25
-
-USER_NAME_FIRST_ERROR: str = (
-    'Укажите правильное имя (например: Иван или Анна-Мария)'
-)
-USER_NAME_LAST_ERROR: str = (
-    'Укажите правильную фамилию (например: Петров или Баронесса-Ивальди фон Бремзен)'
-)
-# INFO: существуют фамилии с одной буквой, необходимо производить
-#       проверку на то, что имя не начинается и не заканчивается на "-".
-# INFO: src.models.user.USER_USERNAME_LEN = 25
-USER_NAME_MAX_REGEXP: str = r'^[А-ЯЁа-яё\s\-]{1,25}$'
-
-USER_PAYMENT_DATA_CARD_NUMBER: int = 4 * 4
-
-# INFO. Номера в БД хранятся в формате набора цифр без иных знаков.
-#       7 - это городской номер без кодов
-#       20 - существующее значение в БД, принят запас по длине.
-USER_PHONE_MAX_LEN: int = 20
-USER_PHONE_REGEX: str = r'^[0-9]{7,20}$'
-USER_PHONE_ERROR: str = (
-    'Введите номер телефона, состоящий от 7 до 20 цифр в формате XХХХХХХХХХХ.'
-)
-USER_PHONE_RUS_ERROR: str = (
-    'Для абонентов РФ необходимо указывать номер в формате 79ХХХХХХХХХ.'
-)
-
-USER_PASS_RAW_LEN_MAX: int = 50
-USER_PASS_RAW_LEN_MIN: int = 8
-PASS_SPECIAL_CHARS: str = '!_@#$%^&+='
-
-PASS_CHARS_VALIDATORS: dict[str, str] = {
-    lambda s: USER_PASS_RAW_LEN_MIN <= len(s) <= USER_PASS_RAW_LEN_MAX: f'{NL}- длина от {USER_PASS_RAW_LEN_MIN} до {USER_PASS_RAW_LEN_MAX} символов',
-    lambda s: any(char.isdigit() for char in s): '\n- включает хотя бы одну цифру (0-9)',
-    lambda s: any(char.islower() for char in s): '\n- включает хотя бы одну прописную букву (a-z)',
-    lambda s: any(char.isupper() for char in s): '\n- включает хотя бы одну заглавную букву (A-Z)',
-    lambda s: any(char in PASS_SPECIAL_CHARS for char in s): f'{NL}- включает хотя бы один специальный символ ({PASS_SPECIAL_CHARS})',
-}
-
-USER_SALESMAN_COMPANY_DESCRIPTION_LEN: int = 200
-USER_SALESMAN_COMPANY_IMAGE_LEN: int = 200
-USER_SALESMAN_COMPANY_NAME_LEN: int = 50
+    DESCRIPTION_LEN_MAX: int = 250
+    NAME_LEN_MAX: int = 30
+    IMAGE_LEN_MAX: int = 200
 
 
-def validate_user_company_description(cls, value: str, values: dict):
+class UserBankCardParams:
+    """Класс с параметрами банковских карт пользователей."""
+
+    CARDHOLDER_LEN_MAX: int = 30
+    NUMBER_LEN_MAX: int = 16
+    TITLE_LEN_MAX: int = 30
+
+
+class UserParams:
+    """Класс с параметрами пользователей."""
+
+    # INFO: в pydantic.EmailStr разрешенные длины строк составляют 64@63.63
+    EMAIL_LEN_MAX: int = 64 + 1 + 63 + 1 + 63
+
+    NAME_FIRST_ERROR: str = 'Укажите правильное имя (например: Иван или Анна-Мария)'
+    NAME_LAST_ERROR: str = 'Укажите правильную фамилию (например: Петров или Баронесса-Ивальди фон Бремзен)'
+    # INFO: существуют фамилии с одной буквой, необходимо производить
+    #       проверку на то, что имя не начинается и не заканчивается на "-".
+    NAME_LEN_MAX: int = 25
+    NAME_REGEXP: str = r'^[А-ЯЁа-яё\s\-]{1,25}$'
+
+    PASSWORD_HASHED_LEN_MAX: int = 256
+    PASSWORD_RAW_LEN_MAX: int = 50
+    PASSWORD_RAW_LEN_MIN: int = 8
+    PASSWORD_SPECIAL_CHARS: str = '!_@#$%^&+='
+
+    # INFO. Номера в БД хранятся в формате набора цифр без иных знаков.
+    #       7 - это городской номер без кодов
+    #       20 - существующее значение в БД, принят запас по длине.
+    PHONE_LEN_MAX: int = 20
+    PHONE_REGEX: str = r'^[0-9]{7,20}$'
+    PHONE_ERROR: str = 'Введите номер телефона, состоящий от 7 до 20 цифр в формате XХХХХХХХХХХ.'
+    PHONE_ERROR_RUS: str = 'Для абонентов РФ необходимо указывать номер в формате 79ХХХХХХХХХ.'
+
+    @classmethod
+    def get_password_validators(cls) -> dict[str, str]:
+        """Возвращает список валидаторов пароля."""
+        return {
+            lambda s: cls.PASSWORD_RAW_LEN_MIN <= len(s) <= cls.PASSWORD_RAW_LEN_MAX:
+                f'\n- длина от {cls.PASSWORD_RAW_LEN_MIN} до {cls.PASSWORD_RAW_LEN_MAX} символов',
+            lambda s: any(char.isdigit() for char in s):
+                '\n- включает хотя бы одну цифру (0-9)',
+            lambda s: any(char.islower() for char in s):
+                '\n- включает хотя бы одну прописную букву (a-z)',
+            lambda s: any(char.isupper() for char in s):
+                '\n- включает хотя бы одну заглавную букву (A-Z)',
+            lambda s: any(char in cls.PASSWORD_SPECIAL_CHARS for char in s):
+                f'\n- включает хотя бы один специальный символ ({cls.PASSWORD_SPECIAL_CHARS})',
+        }
+
+
+def validate_user_company_description(value: str):
     """Производит валидацию описания компании."""
-    if len(value) == 0 or len(value) > COMPANY_DESCRIPTION_MAX:
-        raise ValueError(
-            f'Описание компании должно содержать от 1 до {COMPANY_DESCRIPTION_MAX} символов'
-        )
+    if not value or len(value) > CompanyParams.DESCRIPTION_LEN_MAX:
+        raise ValueError(f'Описание компании должно содержать от 1 до {CompanyParams.DESCRIPTION_LEN_MAX} символов')
     return value
 
 
 def validate_user_company_name(value: str):
     """Производит валидацию названия компании продавца."""
-    if len(value) == 0 or len(value) > COMPANY_NAME_MAX:
-        raise ValueError(
-            f'Название компании должно содержать от 1 до {COMPANY_NAME_MAX} символов'
-        )
+    if not value or len(value) > CompanyParams.NAME_LEN_MAX:
+        raise ValueError(f'Название компании должно содержать от 1 до {CompanyParams.NAME_LEN_MAX} символов')
     return value
 
 
@@ -89,13 +92,13 @@ def validate_user_password(value: str) -> str:
     errors: list[str] = [
         err_message
         for condition, err_message
-        in PASS_CHARS_VALIDATORS.items()
+        in UserParams.get_password_validators().items()
         if not condition(value)
     ]
     if len(errors) > 0:
         raise ValueError(
-            'Введите пароль, который удовлетворяет критериям:' +
-            ''.join(errors)
+            'Ваш пароль не удовлетворят критериям:' +
+            ''.join(errors),
         )
     return value
 
@@ -127,10 +130,10 @@ def validate_user_phone(value: str) -> str:
             +886 - Тайвань
     """
 
-    if not fullmatch(pattern=USER_PHONE_REGEX, string=value):
-        raise ValueError(USER_PHONE_ERROR)
+    if not fullmatch(pattern=UserParams.PHONE_REGEX, string=value):
+        raise ValueError(UserParams.PHONE_ERROR)
     if value.startswith('89'):
-        raise ValueError(USER_PHONE_RUS_ERROR)
+        raise ValueError(UserParams.PHONE_ERROR_RUS)
     return value
 
 
@@ -141,9 +144,25 @@ def validate_user_name(value: str, err: str) -> str:
     Переводит символы поля в title регистр.
     """
     if (
-        not fullmatch(USER_NAME_MAX_REGEXP, value) or
-        value.startswith('-') or
+        not fullmatch(UserParams.NAME_REGEXP, value)
+        or
+        value.startswith('-')
+        or
         value.endswith('-')
     ):
         raise ValueError(err)
     return value.title()
+
+
+def validate_user_new_password(value: str, current_password: str) -> str:
+    """Валидатор для поля "new_password" схемы "AuthPasswordChangeSchema"."""
+    if value and value == current_password:
+        raise ValueError('Прежний и новый пароли должны отличаться.')
+    return validate_user_password(value=value)
+
+
+def validate_user_new_password_confirm(value: str, new_password: str) -> str:
+    """Валидатор для поля "new_password_confirm" схемы "AuthPasswordChangeSchema"."""
+    if value and value != new_password:
+        raise ValueError('Пароли не совпадают.')
+    return value
